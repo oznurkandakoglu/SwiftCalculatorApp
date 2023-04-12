a5b7d320c78d2c42e7a774ecf4c739a41a206dd6
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2023-04-12 11:35:22 +0300

    v 0.2

--- SwiftCalculatorApp/ViewController.swift
+++ SwiftCalculatorApp/ViewController.swift
@@ -9,10 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var firstText: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-//sa
+    @IBAction func sumClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+    }
+      
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstNumber = Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!){
+                let result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    
 }
 
